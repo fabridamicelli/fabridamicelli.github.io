93b18b6b826c76af403211283228dc445d5a69bf
Fabrizio Damicelli <fabridamicelli@gmail.com>
2024-07-22 15:43:29 +0200

    add last cell with script

---
+++ scripts/add_last_cell_to_posts.py
@@ -0,0 +1,15 @@
+import json
+from pathlib import Path
+
+POSTS_PATH = Path(".")/"posts"
+LAST_CELL_PATH = Path(".")/"fin.json"
+last_cell = json.loads(LAST_CELL_PATH.read_text())
+
+nb_paths = [p for p in POSTS_PATH.iterdir() if p.name.endswith(".ipynb")]
+
+for path in nb_paths:
+    print(f"Adding last cell to {path.name}")
+    nb = json.loads(path.read_text())
+    nb["cells"][-1] = last_cell
+    path.write_text(json.dumps(nb))
+

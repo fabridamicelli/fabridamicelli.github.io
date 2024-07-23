58bbba18738ed3d6c69c2e9b445654188340460b
Fabrizio Damicelli <fabridamicelli@gmail.com>
2024-07-23 17:54:12 +0200

    add post-template

--- scripts/new-post.sh
+++ scripts/new-post.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
-cd scripts
-
 echo "Enter notebook name:"
 name=$(python -c 'n=input(); print(n)')
 d=$(date +"%Y-%m-%d")
-fullname="${d}-${name}.ipynb"
+fullname="posts/${d}-${name}.ipynb"
 
-cp "../template.ipynb" "../posts/${fullname}"
+cp "post-template.ipynb" "${fullname}"
 
 echo "Created ${fullname}"
 
-cd - > /dev/null
+jupyter lab --browser=google-chrome --notebook-dir=posts "${fullname}"
